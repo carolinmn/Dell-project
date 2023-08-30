0e13381c386852202ddcd993bab15aff26584482
Ahmad Agbaria <ah.agbaria.99@gmail.com>
2023-08-30 13:48:27 +0300

    add dispacher file

---
+++ logs_analyzer/src/dispatcher/page/Home.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Home.css";
+import Header from "../../comp/header";
+import Sidebar from "../../comp/Sidebar";
+
+const Home = (OpenSidebar, openSidebarToggle) => {
+
+  
+  return (
+    
+    <div className="App">
+      <Sidebar
+        openSidebarToggle={openSidebarToggle}
+        OpenSidebar={OpenSidebar}
+      />
+
+      <div className="App2">
+        <Header OpenSidebar={OpenSidebar} pagename="dispatcher Page"  />
+
+        <main className="main-content">
+            
+          Hello there
+          {/*change the name of file and start Work  */}
+
+
+        </main>
+
+
+      </div>
+    </div>
+  );
+};
+
+export default Home;
