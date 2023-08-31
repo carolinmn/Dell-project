8f06d7c0965fa1c8e9c25bd264fed7a134109bdc
nawras <55401773+rabiaaNawras99@users.noreply.github.com>
2023-08-31 13:31:30 +0300

    whatever
    qwertyuiopfds

--- logs_analyzer/src/AdminDashboard/comp/AdminHeader.js
+++ logs_analyzer/src/AdminDashboard/comp/AdminHeader.js
@@ -1,20 +1,17 @@
 import React from "react";
 import "../../comp/header.css";
 
-
 const AdminHeader = () => {
   return (
     <header className="header">
-    
-        <div className="leftheader">
-          <h3>Admin Dashboard </h3>
-        </div>
-        <div className="rightheader">
-          <i class="fa-regular fa-bell"></i>
-          <img src=""></img>
-          <span>Ahmad Agbaria</span>
-        </div>
-    
+      <div className="leftheader">
+        <h3>Admin Dashboard </h3>
+      </div>
+      <div className="rightheader">
+        <i class="fa-regular fa-bell"></i>
+        <img src=""></img>
+        <span>Ahmad Agbaria</span>
+      </div>
     </header>
   );
 };
