292d7d93d98dc61dfb43ee67e06143c624da3982
Ahmad Agbaria <ah.agbaria.99@gmail.com>
2023-09-13 19:02:11 +0300

    added notification container

--- logs_analyzer/src/AdminDashboard/comp/AdminHeader.js
+++ logs_analyzer/src/AdminDashboard/comp/AdminHeader.js
@@ -2,22 +2,58 @@ import React, { useState } from "react";
 import "../../styles/header.css";
 
 const AdminHeader = (props) => {
+
+  // fetch the data from the backend
   const [data, setData] = useState({
     _id: "1",
     userName: "Axwara",
     firstName: "Ahmad",
     lastName: "Agbaria",
-    imageURL: "https://th-thumbnailer.cdn-si-edu.com/5V-xOO-B6H14VTVruDbqnMvmmE0=/fit-in/1072x0/filters:focal(342x433:343x434)/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer/e4/af/e4afdbe6-59c0-4a6b-8112-8b30e274d9a0/2009-49519-h-floresiensis-jgurche_web.jpg",
+    imageURL:
+      "https://th-thumbnailer.cdn-si-edu.com/5V-xOO-B6H14VTVruDbqnMvmmE0=/fit-in/1072x0/filters:focal(342x433:343x434)/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer/e4/af/e4afdbe6-59c0-4a6b-8112-8b30e274d9a0/2009-49519-h-floresiensis-jgurche_web.jpg",
   });
 
-  
+  const [notifications, setNotifications] = useState([
+    {
+      _id: "1",
+      from: "x",
+      message:
+        "message 1 dsdsdsdsd message 1 dsdsdsdsd message 1sdsds dsdsdsdsd",
+    },
+    {
+      _id: "2",
+      from: "y",
+      message: "message 2",
+    },
+    {
+      _id: "3",
+      from: "z",
+      message: "message 3",
+    },
+    {
+      _id: "4",
+      from: "x",
+      message: "message 1 dsdsdsdsd",
+    },
+    {
+      _id: "5",
+      from: "y",
+      message: "message 2",
+    },
+  ]);
 
   const [compUploadPic, setCompUploadPic] = useState(false);
   const [newImageURL, setNewImageURL] = useState("");
+  const [compNotifications, setCompNotifications] = useState(false);
 
   const toggleUploadPic = () => {
-    // Use functional update to toggle the state
-    setCompUploadPic(prevState => !prevState);
+    if (compNotifications) setCompNotifications(false);
+    setCompUploadPic((prevState) => !prevState);
+  };
+
+  const toggleNotifications = () => {
+    if (compUploadPic) setCompUploadPic(false);
+    setCompNotifications((prevState) => !prevState);
   };
 
   const handleImageUpload = () => {
@@ -27,6 +63,13 @@ const AdminHeader = (props) => {
     }
   };
 
+  const handleDeleteNotification = (id) => {
+    setNotifications((prevNotifications) =>
+      prevNotifications.filter((notif) => notif._id !== id)
+    );
+    // we need to fetch to backend
+  };
+
   return (
     <header>
       <div className="header">
@@ -34,12 +77,8 @@ const AdminHeader = (props) => {
           <h3>{props.role} Dashboard</h3>
         </div>
         <div className="rightheader">
-          <i className="fa-regular fa-bell"></i>
-          <img
-            src={data.imageURL}
-            alt="img"
-            onClick={toggleUploadPic}
-          />
+          <i className="fa-regular fa-bell" onClick={toggleNotifications}></i>
+          <img src={data.imageURL} alt="img" onClick={toggleUploadPic} />
           <span>
             {data.firstName} {data.lastName}
           </span>
@@ -53,10 +92,43 @@ const AdminHeader = (props) => {
             value={newImageURL}
             onChange={(event) => setNewImageURL(event.target.value)}
           />
-          <br/>
+          <br />
           <div className="buttons">
             <button onClick={handleImageUpload}>OK</button>
             <button onClick={() => setCompUploadPic(false)}>Cancel</button>
+          </div>
+        </div>
+      )}
+
+      {compNotifications && (
+        <div className="notf-container">
+          <div className="notf-header">
+            <h4>Notifications</h4>
+            <i className="fa-solid fa-times" onClick={toggleNotifications}></i>
+          </div>
+          <div className="notf-list">
+            {notifications.length > 0 ? (
+              <div className="scrollable-notifications">
+                {notifications.map((notif) => (
+                  <div className="notification" key={notif._id}>
+                    <div className="notification-content">
+                      <div>
+                        <strong>From: {notif.from}</strong>
+                      </div>
+                      <div>{notif.message}</div>
+                    </div>
+                    <button
+                      className="delete-button"
+                      onClick={() => handleDeleteNotification(notif._id)}
+                    >
+                      Delete
+                    </button>
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <p>No notifications</p>
+            )}
           </div>
         </div>
       )}
